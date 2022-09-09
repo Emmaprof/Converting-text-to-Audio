f2f3c37affdc58e3f91a5e51a3ddde6ca49a635d
Emmaprof <emmanuelesin950@gmail.com>
2022-09-09 23:19:12 +0100

    first commit

---
+++ Text to Audio.py
@@ -0,0 +1,9 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+
+engine.say("hello senior developer EMMANUEL, how is your coding going?")
+#engine.say("welcome to pyttsx3")
+engine.say("my name is Emmanuel\n I am a senior developer")
+
+engine.runAndWait()
